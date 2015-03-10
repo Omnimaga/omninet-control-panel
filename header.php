cad71bf06e8d954dfe43d191f2f189a3cdec3b99
Nathaniel van Diepen <eeems@omnimaga.org>
2015-03-10 13:50:13 -0600

    Memory limit changes for Omnimaga's implementation

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <?php
 	error_reporting(E_ALL);
 	ini_set("display_errors", 1);
+	ini_set('memory_limit','512M');
 	session_start();
 	define('DIR',dirname(__FILE__));
 	require_once(DIR.'/lib/locale.php');
